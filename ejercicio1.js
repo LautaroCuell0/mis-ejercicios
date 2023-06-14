488131e9911d4ddec76697c0ecdf7cf7616bbfa7
lautaro cuello <lautarocuello630@gmail.com>
2023-06-14 19:24:24 -0300

    se agrego la tarea de la fecha 12/06

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,8 +1,34 @@
-function hola(){
-   alert('hola')
-}
+// function hola(){
+//    alert('hola')
+// }
 
-hola()
+// hola()
 
 
-alert('adios')
+// alert('adios')
+
+
+// 1 Escribe un programa que imprima "¡Hola, mundo!" en la consola.
+
+// 2 Crea un programa que solicite al usuario su nombre y apellido en dos variables y luego lo salude por su completo.
+
+// 3 Escribe un programa que calcule la suma de dos números ingresados por el usuario y muestre el resultado.
+
+
+// 1-
+
+//EJERCICIOS DE LA CLASE 12/06//
+
+ console.log('hola, mundo')
+
+ let nombre = prompt('introduce tu nombre')
+ let apellido = prompt ('introduce tu apellido')
+
+ console.log('Hola, buenas '+ nombre + " " + apellido)
+
+ let numero1 = parseInt(prompt('introduce un numero'))
+ let numero2 = parseInt(prompt('introduce un numero'))
+
+let suma = (numero1 + numero2)
+
+console.log(suma)
