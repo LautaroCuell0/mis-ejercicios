c62095fd1eddb991c569a2bc71a940192b91833f
lautaro cuello <lautarocuello630@gmail.com>
2023-06-21 21:27:08 -0300

    se agregaron los ejercicios de la clase 21-06

---
+++ condicionales.js
@@ -0,0 +1,35 @@
+// let primerNumero = parseInt(prompt('ingresa el primer numero'))
+// let segundoNumero = parseInt(prompt('ingresa el segundo numero'))
+
+// if (primerNumero >= segundoNumero){
+//     console.log(`el primer numero ${primerNumero} es mayor o igual que el segundo, el cual es ${segundoNumero}`)
+// // } else if(primerNumero = segundoNumero){
+// //     console.log(`el primer numero ${primerNumero} es igual al segundo, el cual es ${segundoNumero}`)
+// // 
+// } 
+// else {
+//     console.log(`el primer numero ${primerNumero} es menor que el segundo, el cual es ${segundoNumero}`)
+// }
+
+//PEDIR NUMERO AL USUARIO Y VER SI ES PAR O IMPAR//
+
+// let numero = parseInt(prompt('introduce el numero'))
+
+// if (numero % 2 === 0){
+//     console.log(`el numero ${numero} es par`)
+// } else {
+//     console.log(`el numero ${numero} es impar`)
+// }
+
+
+// pregunta la edad, si es mayor deja pasar a la fiesta pero si no consutal si viene con cun tutori sino no dejar pasar//
+
+let edad = parseInt(prompt('ingrese tu edad'))
+let tutor = prompt('tienes tutor?')
+if (edad>=18){
+    console.log(`${edad} tu edad es la indicada para pasar`)
+} else if (edad<18 && tutor.includes('si')){
+   console.log(`tu edad ${edad} esta debajo de la minima pero entras con tu tutor`)
+}else{
+    console.log(`tu edad ${edad} no es la indicada para pasar, si no cuentas con un tutor no podras pasar`)
+}
