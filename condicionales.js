aec3d238c54502af12cf6bb7a910dd7d75e36406
lautaro cuello <lautarocuello630@gmail.com>
2023-06-27 22:03:36 -0300

    se agrego ejercicio de 26-06

--- condicionales.js
+++ condicionales.js
@@ -24,14 +24,30 @@
 
 // pregunta la edad, si es mayor deja pasar a la fiesta pero si no consutal si viene con cun tutori sino no dejar pasar//
 
-let edad = parseInt(prompt('ingrese tu edad'))
-let tutor = prompt('tienes tutor?')
-if (edad>=18){
-    console.log(`${edad} tu edad es la indicada para pasar`)
-} else if (edad<18 && tutor.toLowerCase().includes('si')){
-   console.log(`tu edad ${edad} esta debajo de la minima pero entras con tu tutor`)
-} else if(edad <= 10){
-   console.log('deberias ir a una guarderia y no estar acá')
-} else{
-    console.log(`tu edad ${edad} no es la indicada para pasar, si no cuentas con un tutor no podras pasar`)
+// let edad = parseInt(prompt('ingrese tu edad'))
+// let tutor = prompt('tienes tutor?')
+// if (edad>=18){
+//     console.log(`${edad} tu edad es la indicada para pasar`)
+// } else if (edad<18 && tutor.toLowerCase().includes('si')){
+//    console.log(`tu edad ${edad} esta debajo de la minima pero entras con tu tutor`)
+// } else if(edad <= 10){
+//    console.log('deberias ir a una guarderia y no estar acá')
+// } else{
+//     console.log(`tu edad ${edad} no es la indicada para pasar, si no cuentas con un tutor no podras pasar`)
+// }
+
+
+//Una persona esta por comprar inmuebles, si lleva anteojos podra leer en la letra pequeña de se avisa
+//que el inmueble esta construido sobre un cementerio indio embrujado
+
+let lentes = prompt('traes lentes?').toLocaleLowerCase()
+
+switch (lentes) {
+    case 'si': console.log('corre, te van a asesinar los fantasmas!!!!')
+        break;
+    case 'no': console.log('compralo YAAA!!!')
+        break
+    default:
+        console.log('responde si traes lentes con si o no')
+        break;
 }
