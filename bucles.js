a48da2c68189e514d31badaa349a7c7ca5f8189e
lautaro cuello <lautarocuello630@gmail.com>
2023-07-09 20:24:50 -0300

    se agregaron ejercicios del tp1, falta el ej 9

---
+++ bucles.js
@@ -0,0 +1,58 @@
+
+
+// //PORQUE LA LUNA ES MAYOR QUE EL SOL//
+
+// //let pregunta = prompt('PORQUE LA LUNA ES MAYOR QUE EL SOL???').toLocaleLowerCase();
+
+//  //while (pregunta !== 'porque la dejan salir de noche') {
+//     pregunta = prompt('segui intentando')
+//  //}
+
+// // let nameUser;
+// // let king = false;
+
+// // while (nameUser !== 'arturo' || king == true){
+// //    let nameUser = prompt('ingresa tu nombre');
+// //    if(nameUser == 'arturo'){
+// //       king == confirm
+// //       if(king == false){
+// //          alert('no eres el rey digno')
+// //       }
+// //    }
+// // }
+// // console.log('bienvenido rey')
+
+// // let ingresaNumero = prompt('ingresa numero deseado')
+
+// // for(let i = 0; i<=10; i++){
+// //    console.log(`${ingresaNumero} X ${i} = ${ingresaNumero * i}`)
+// // } 
+ let continuar = true
+ let suma = 0
+
+ while(continuar == true){
+       let numero =parseInt(dprompt('numero'))
+    if(numero == null) {
+       continuar =false
+       alert('gracias por colaborar')
+       console.log(suma+numero)
+       
+//       let otroNumero = confirm('desea agregar mas numeros?')   
+//     if(otroNumero == true){
+//        otroNumero = numero
+//        otroNumero = parseInt(prompt('introduce un numero'))
+//     }else{
+//        otroNumero = false
+//        continuar = false
+//        alert('gracias por colaborar') 
+//        console.log(suma+numero)
+//     }
+ } 
+ }
+ 
+
+
+
+
+
+
--- condicionales.js
+++ condicionales.js
@@ -53,11 +53,49 @@
 // }
 
 
-//PORQUE LA LUNA ES MAYOR QUE EL SOL//
 
-let pregunta = prompt('PORQUE LA LUNA ES MAYOR QUE EL SOL???').toLocaleLowerCase();
+// //PORQUE LA LUNA ES MAYOR QUE EL SOL//
 
- while (pregunta !== 'porque la dejan salir de noche') {
-    pregunta = prompt('segui intentando')
- }
+// //let pregunta = prompt('PORQUE LA LUNA ES MAYOR QUE EL SOL???').toLocaleLowerCase();
 
+//  //while (pregunta !== 'porque la dejan salir de noche') {
+//     pregunta = prompt('segui intentando')
+//  //}
+
+// // let nameUser;
+// // let king = false;
+
+// // while (nameUser !== 'arturo' || king == true){
+// //    let nameUser = prompt('ingresa tu nombre');
+// //    if(nameUser == 'arturo'){
+// //       king == confirm
+// //       if(king == false){
+// //          alert('no eres el rey digno')
+// //       }
+// //    }
+// // }
+// // console.log('bienvenido rey')
+
+// // let ingresaNumero = prompt('ingresa numero deseado')
+
+// // for(let i = 0; i<=10; i++){
+// //    console.log(`${ingresaNumero} X ${i} = ${ingresaNumero * i}`)
+// // }
+
+// let continuar = true
+// let suma = 0
+
+// while(continuar == true){
+//       let numero =prompt('numero')
+//    if(numero == null) {
+//       let otroNumero = confirm('desea agregar mas numeros?')   
+//    if(otroNumero == true){
+//       otroNumero = prompt('introduce un numero')
+//    }else{
+//       otroNumero = false
+//       continuar = false
+//       alert('gracias por colaborar') 
+//    }
+// } 
+// }
+// console.log(suma)
